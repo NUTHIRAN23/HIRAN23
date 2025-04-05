6187ae3b13262327fecb85288dac88dd6d699481
NUTHIRAN23 <nut.hiran23@gmail.com>
2025-04-05 21:03:12 +0700

    Update graphics.js

--- graphics.js
+++ graphics.js
@@ -23,9 +23,9 @@ function updateRocketPosition() {
   let targetY = currentFrame.target.y;
   let targetAngle = currentFrame.angle;
 
-  let newX = currentX + (targetX - currentX) * 0.01; 
-  let newY = currentY + (targetY - currentY) * 0.01;
-  currentAngle += (targetAngle - currentAngle) * 0.01;
+  let newX = currentX + (targetX - currentX) * 0.005; 
+  let newY = currentY + (targetY - currentY) * 0.005;
+  currentAngle += (targetAngle - currentAngle) * 0.005;
 
   rocket.style.transform = `translate(${newX}px, ${newY}px) rotate(${currentAngle}deg)`;
 
